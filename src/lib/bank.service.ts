c08dfc35266477e5aa692f474f8d62430c0117cf
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-11 01:02:58 +0000

    Refactored Bank Service

--- src/lib/bank.service.ts
+++ src/lib/bank.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BankData, PaginatedData, ATMData, ManagerData } from './bank.models';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BankService {
+  private ourBaseUrl = 'https://atm-hotspot-backend.herokuapp.com';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMyAccount(): Observable<BankData> {
+    return this.http.get<BankData>(`${this.ourBaseUrl}/banks/me`);
+  }
+
+  getATMs(
+    url: string = `${this.ourBaseUrl}/banks/me/atms`
+  ): Observable<PaginatedData<ATMData>> {
+    return this.http.get<PaginatedData<ATMData>>(url);
+  }
+
+  addATM($data: {
+    name: string;
+    status: number;
+    city: string;
+    lat: number;
+    lng: number;
+  }): Observable<ATMData> {
+    return this.http.post<ATMData>(`${this.ourBaseUrl}/banks/me/atms`, $data);
+  }
+
+  getManagers(
+    url: string = `${this.ourBaseUrl}/banks/me/managers`
+  ): Observable<PaginatedData<ManagerData>> {
+    return this.http.get<PaginatedData<ManagerData>>(url);
+  }
 }
