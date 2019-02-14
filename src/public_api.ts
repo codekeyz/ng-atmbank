a7bb6d7fed0d11db4626425cbfee7bc1e41c92a9
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-14 16:21:53 +0000

    added billing-service to public_apis

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of bank
  */
 
-export * from './lib/bank.service';
-export * from './lib/bank-auth.service';
-export * from './lib/bank.module';
+export * from './lib/services/bank.service';
+export * from './lib/services/bank-auth.service';
+export * from './lib/services/bank-billing.service';
+export * from './lib/modules/bank.module';
