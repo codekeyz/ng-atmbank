5528f7438e7f75485889c4a89390c44b35281dcf
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-16 00:39:49 +0000

    Added BankBilling-Service

--- src/lib/services/bank-billing.service.ts
+++ src/lib/services/bank-billing.service.ts
@@ -1,26 +1,25 @@
-import {Inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {StripeService} from 'ngx-stripe';
-import {BankModule, Config} from '../modules/bank.module';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BankModule, Config } from '../modules/bank.module';
+import { PlanData } from '../bank.models';
 
 @Injectable({
   providedIn: BankModule
 })
 export class BankBillingService {
-
   private ourBaseUrl = '';
 
   constructor(
     @Inject('config') private config: Config,
-    private http: HttpClient,
-    private stripeSvc: StripeService
+    private http: HttpClient
   ) {
     config.isDevMode === true
       ? (this.ourBaseUrl = 'https://atm-hotspot-backend.herokuapp.com')
       : (this.ourBaseUrl = 'http://127.0.0.1:8000');
   }
 
-  getPlans() {
+  getPlans(): Observable<PlanData[]> {
+    return this.http.get<PlanData[]>(`${this.ourBaseUrl}/banks/plans`);
   }
-
 }
