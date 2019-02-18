c0041a5b88c5dbd81e93aea793d49b54c4f1c2b3
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-18 14:11:02 +0000

    Refactored functions in BankService

--- src/lib/services/bank.service.ts
+++ src/lib/services/bank.service.ts
@@ -1,8 +1,16 @@
-import {Inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {BankModule, Config} from '../modules/bank.module';
-import {APIResponse, ATMData, BankData, BranchData, Data, ManagerData, PaginatedData} from '../bank.models';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BankModule, Config } from '../modules/bank.module';
+import {
+  APIResponse,
+  ATMData,
+  BankData,
+  BranchData,
+  Data,
+  ManagerData,
+  PaginatedData
+} from '../bank.models';
 
 @Injectable({
   providedIn: BankModule
@@ -23,43 +31,83 @@ export class BankService {
     return this.http.get<Data<BankData>>(`${this.ourBaseUrl}/banks/me`);
   }
 
-  getBranches(url: string = `${this.ourBaseUrl}/banks/me/branches`): Observable<PaginatedData<BranchData>> {
-    return this.http.get<PaginatedData<BranchData>>(url);
+  getBranches(params?: {
+    paginate?: number;
+    page?: number;
+  }): Observable<PaginatedData<BranchData>> {
+    return this.http.get<PaginatedData<BranchData>>(
+      `${this.ourBaseUrl}/banks/me/branches`,
+      {
+        params:
+          params === undefined
+            ? {}
+            : {
+                paginate: params.paginate.toString(),
+                page: params.page.toString()
+              }
+      }
+    );
   }
 
   getBranch(id: string): Observable<PaginatedData<BranchData>> {
-    return this.http.get<PaginatedData<BranchData>>(`${this.ourBaseUrl}/banks/me/branches/${id}`);
+    return this.http.get<PaginatedData<BranchData>>(
+      `${this.ourBaseUrl}/banks/me/branches/${id}`
+    );
   }
 
   addBranch($data: {
-    name: string,
-    city: string,
-    town?: string
+    name: string;
+    city: string;
+    town?: string;
   }): Observable<Data<BranchData>> {
-    return this.http.post<Data<BranchData>>(`${this.ourBaseUrl}/banks/me/branches`, $data);
+    return this.http.post<Data<BranchData>>(
+      `${this.ourBaseUrl}/banks/me/branches`,
+      $data
+    );
   }
 
-  updateBranch(id: string, $data: {
-    name?: string,
-    city?: string,
-    town?: string
-  }): Observable<Data<BranchData>> {
-    return this.http.put<Data<BranchData>>(`${this.ourBaseUrl}/banks/me/branches/${id}`, $data);
+  updateBranch(
+    id: string,
+    $data: {
+      name?: string;
+      city?: string;
+      town?: string;
+    }
+  ): Observable<Data<BranchData>> {
+    return this.http.put<Data<BranchData>>(
+      `${this.ourBaseUrl}/banks/me/branches/${id}`,
+      $data
+    );
   }
 
   deleteBranch(id: string): Observable<APIResponse> {
-    return this.http.delete<APIResponse>(`${this.ourBaseUrl}/banks/me/branches/${id}`);
+    return this.http.delete<APIResponse>(
+      `${this.ourBaseUrl}/banks/me/branches/${id}`
+    );
   }
 
-  getATMs(
-    url: string = `${this.ourBaseUrl}/banks/me/atms`
-  ): Observable<PaginatedData<ATMData>> {
-    return this.http.get<PaginatedData<ATMData>>(url);
+  getATMs(params?: {
+    paginate?: number;
+    page?: number;
+  }): Observable<PaginatedData<ATMData>> {
+    return this.http.get<PaginatedData<ATMData>>(
+      `${this.ourBaseUrl}/banks/me/atms`,
+      {
+        params:
+          params === undefined
+            ? {}
+            : {
+                paginate: params.paginate.toString(),
+                page: params.page.toString()
+              }
+      }
+    );
   }
-
 
   getATM(id: string): Observable<Data<ATMData>> {
-    return this.http.get<Data<ATMData>>(`${this.ourBaseUrl}/banks/me/atms/${id}`);
+    return this.http.get<Data<ATMData>>(
+      `${this.ourBaseUrl}/banks/me/atms/${id}`
+    );
   }
 
   addATM($data: {
@@ -68,6 +116,7 @@ export class BankService {
     city: string;
     lat: number;
     lng: number;
+    branch_id: number;
   }): Observable<Data<ATMData>> {
     return this.http.post<Data<ATMData>>(
       `${this.ourBaseUrl}/banks/me/atms`,
@@ -83,6 +132,7 @@ export class BankService {
       city?: string;
       lat?: number;
       lng?: number;
+      branch_id?: number;
     }
   ): Observable<Data<ATMData>> {
     return this.http.put<Data<ATMData>>(
@@ -92,23 +142,40 @@ export class BankService {
   }
 
   deleteATM(id: string): Observable<APIResponse> {
-    return this.http.delete<APIResponse>(`${this.ourBaseUrl}/banks/me/atms/${id}`);
+    return this.http.delete<APIResponse>(
+      `${this.ourBaseUrl}/banks/me/atms/${id}`
+    );
   }
 
-  getManagers(
-    url: string = `${this.ourBaseUrl}/banks/me/managers`
-  ): Observable<PaginatedData<ManagerData>> {
-    return this.http.get<PaginatedData<ManagerData>>(url);
+  getManagers(params?: {
+    paginate?: number;
+    page?: number;
+  }): Observable<PaginatedData<ManagerData>> {
+    return this.http.get<PaginatedData<ManagerData>>(
+      `${this.ourBaseUrl}/banks/me/managers`,
+      {
+        params:
+          params === undefined
+            ? {}
+            : {
+                paginate: params.paginate.toString(),
+                page: params.page.toString()
+              }
+      }
+    );
   }
 
   getManager(id: string): Observable<PaginatedData<ManagerData>> {
-    return this.http.get<PaginatedData<ManagerData>>(`${this.ourBaseUrl}/banks/me/managers/${id}`);
+    return this.http.get<PaginatedData<ManagerData>>(
+      `${this.ourBaseUrl}/banks/me/managers/${id}`
+    );
   }
 
   addManager($data: {
     name: string;
     email: string;
     password: string;
+    branch_id: number;
   }): Observable<Data<ManagerData>> {
     return this.http.post<Data<ManagerData>>(
       `${this.ourBaseUrl}/banks/me/managers`,
@@ -121,6 +188,7 @@ export class BankService {
     $data: {
       name?: string;
       email?: string;
+      branch_id?: number;
     }
   ): Observable<Data<ManagerData>> {
     return this.http.put<Data<ManagerData>>(
@@ -130,6 +198,8 @@ export class BankService {
   }
 
   deleteManager(id: string): Observable<APIResponse> {
-    return this.http.delete<APIResponse>(`${this.ourBaseUrl}/banks/me/managers/${id}`);
+    return this.http.delete<APIResponse>(
+      `${this.ourBaseUrl}/banks/me/managers/${id}`
+    );
   }
 }
